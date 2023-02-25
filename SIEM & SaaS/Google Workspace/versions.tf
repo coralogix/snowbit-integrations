21d4433395ccfa1a0422447969e3b6b82746fbe9
Nir Limor <nir.limor@coralogix.com>
2023-02-25 22:04:25 +0200

    adding Google Workspace integration terraform + readme file

--- SIEM & SaaS/Google Workspace/versions.tf
+++ SIEM & SaaS/Google Workspace/versions.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 4"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3"
