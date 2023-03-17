039d2bf95e6b6675cfa2c48f049a1f35b8910ebc
Nir Limor <nir.limor@coralogix.com>
2023-03-17 10:16:59 +0200

    Google Workspace mod

--- SIEM & SaaS/Google Workspace/aws.tf
+++ SIEM & SaaS/Google Workspace/aws.tf
@@ -1,11 +1,12 @@
 # AWS Variables --->
 variable "ssh_key" {
   type = string
+  default = ""
 }
 variable "subnet_id" {
   type = string
   validation {
-    condition     = can(regex("^subnet\\-[a-f0-9]+", var.subnet_id))
+    condition     = var.subnet_id == "" ? true : can(regex("^subnet\\-[a-f0-9]+", var.subnet_id))
     error_message = "Invalid subnet ID"
   }
 }
@@ -29,70 +30,9 @@ variable "ec2_volume_encryption" {
 variable "SSHIpAddress" {
   type = string
 }
-variable "filebeat_certificates_map_url" {
-  type    = map(string)
-  default = {
-    Europe    = "https://coralogix-public.s3-eu-west-1.amazonaws.com/certificate/"
-    India     = "https://coralogix-public.s3-eu-west-1.amazonaws.com/certificate/"
-    US        = "https://www.amazontrust.com/repository/AmazonRootCA1.pem"
-    Singapore = "https://www.amazontrust.com/repository/AmazonRootCA1.pem"
-  }
-}
-variable "filebeat_certificate_map_file_name" {
-  type    = map(string)
-  default = {
-    Europe    = "Coralogix-EU.crt"
-    India     = "Coralogix-IN.pem"
-    US        = "AmazonRootCA1.pem"
-    Singapore = "AmazonRootCA1.pem"
-  }
-}
-variable "logstash_map" {
-  type    = map(string)
-  default = {
-    Europe    = "logstashserver.coralogix.com"
-    India     = "logstash.app.coralogix.in"
-    US        = "logstashserver.coralogix.us"
-    Singapore = "logstashserver.coralogixsg.com"
-  }
-}
-variable "coralogix_domain" {
-  type    = string
-  default = "Europe"
-  validation {
-    condition     = can(regex("^(?:India|Singapore|Europe|US)$", var.coralogix_domain))
-    error_message = "Invalid Coralogix domain"
-  }
-}
-variable "primary_google_workspace_admin_email_address" {
-  type = string
-  validation {
-    condition     = can(regex("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)$", var.primary_google_workspace_admin_email_address))
-    error_message = "Invalid email address"
-  }
-}
-variable "coralogix_private_key" {
-  type = string
-  validation {
-    condition     = can(regex("^[a-f0-9]{8}\\-(?:[a-f0-9]{4}\\-){3}[a-f0-9]{12}$", var.coralogix_private_key))
-    error_message = "The PrivateKey should be valid UUID string"
-  }
-}
-variable "coralogix_application_name" {
-  type = string
-}
-variable "coralogix_subsystem_name" {
-  type = string
-}
-variable "coralogix_company_id" {
-  type = string
-  validation {
-    condition     = can(regex("^\\d{5,7}$", var.coralogix_company_id))
-    error_message = "Invalid company ID"
-  }
-}
 # AWS Data --->
 data "aws_subnet" "filebeat_subnet" {
+  count = var.subnet_id == "" ? 0 : 1
   id = var.subnet_id
 }
 data "http" "external-ip-address" {
@@ -100,6 +40,7 @@ data "http" "external-ip-address" {
 }
 # AWS Resources --->
 resource "aws_instance" "filebeat_instance" {
+  count = var.instance_cloud_provider == "AWS" ? 1 : 0
   ami                         = "ami-06d94a781b544c133"
   key_name                    = var.ssh_key
   instance_type               = "t3a.small"
@@ -191,8 +132,8 @@ EOT
 }
 resource "aws_security_group" "SecurityGroup" {
   name        = "Security-Group-${random_string.id.id}"
-  count       = length(var.security_group_id) > 0 ? 0 : 1
-  vpc_id      = data.aws_subnet.filebeat_subnet.vpc_id
+  count       = length(var.security_group_id) > 0 ? 0 : var.instance_cloud_provider == "AWS" ? 1 : 0
+  vpc_id      = data.aws_subnet.filebeat_subnet[0].vpc_id
   description = "A security group for Snowbit Integration"
   tags        = merge(var.additional_tags, {
     Terraform-ID = random_string.id.id
@@ -217,9 +158,4 @@ resource "aws_security_group" "SecurityGroup" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-resource "random_string" "id" {
-  length  = 6
-  upper   = false
-  special = false
 }
--- SIEM & SaaS/Google Workspace/values.auto.tfvars
+++ SIEM & SaaS/Google Workspace/values.auto.tfvars
@@ -1,23 +1,24 @@
-# Coralogix
-coralogix_private_key      = ""
-coralogix_application_name = ""
-coralogix_subsystem_name   = ""
-coralogix_company_id       = ""
-coralogix_domain           = ""                   # Europe, India, Singapore or US
+# General - Mandatory
+coralogix_private_key                        = ""
+coralogix_application_name                   = ""
+coralogix_subsystem_name                     = ""
+coralogix_company_id                         = ""
+coralogix_domain                             = ""      # Europe, India, Singapore or US
+instance_cloud_provider                      = ""      # 'AWS' or 'GCP'
+primary_google_workspace_admin_email_address = ""      # in the Google Workspace console under 'Account > Account Settings'
 
-# Google Workspace
-primary_google_workspace_admin_email_address = "" # in the Google Workspace console under 'Account > Account Settings'
-
-# GCP
-existing_project_id          = ""                 # Optional when filling new project information below
-service_account_id           = ""
+# GCP - Mandatory
+existing_project_id          = ""                      # Optional when filling new project information below
 service_account_display_name = ""
-# If a new project is required                    # Optional
-new_project_name             = ""                 # Optional
-new_project_organization     = ""                 # Optional
-new_project_billing_account  = ""                 # Optional
+# GCP - Optional - If a new project is required
+new_project_name             = ""
+new_project_organization     = ""                      # When creating a new project
+new_project_billing_account  = ""                      # When creating a new project
+# When using instance in GCP
+machine_type                 = ""                      # Defaults to 'e2-highcpu-2'
+boot_disk_type               = ""                      # Defaults to 'pd-balanced'
 
-# AWS
+# When using instance in AWS - Optional
 ssh_key               = ""
 subnet_id             = ""
 public_instance       = true
