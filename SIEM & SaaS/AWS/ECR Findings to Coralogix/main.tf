27ca69d2d88648cc0fe0eb6503420f6f9a4b0a12
Nir Limor <nir.limor@coralogix.com>
2023-04-17 14:43:28 +0300

    ecr findings to Coralogix

--- SIEM & SaaS/AWS/ECR Findings to Coralogix/main.tf
+++ SIEM & SaaS/AWS/ECR Findings to Coralogix/main.tf
@@ -11,7 +11,7 @@ variable "subsystem_name" {
 variable "coralogix_domain" {
   type = string
   validation {
-    condition     = can(regex("^Europe|Europe2|India|Singapore|US"))
+    condition     = can(regex("^Europe|Europe2|India|Singapore|US", var.coralogix_domain))
     error_message = "Invalid Coralogix domain."
   }
 }
@@ -33,28 +33,69 @@ locals {
 data "aws_region" "current" {}
 data "aws_caller_identity" "this" {}
 
+// Lambda
+resource "aws_lambda_layer_version" "requests" {
+  layer_name          = "requests"
+  s3_bucket           = "snowbit-shared-resources"
+  s3_key              = "lambda/layers/requests.zip"
+  compatible_runtimes = ["python3.8", "python3.9"]
+}
 resource "aws_lambda_function" "this" {
-  function_name = "ECR Findings to Coralogix"
+  function_name = "ECR-Findings-to-Coralogix-${random_string.this.id}"
   description   = "Sends ECR image scan findings to Coralogix"
   role          = aws_iam_role.this.arn
-  handler       = "python3.9"
+  handler       = "main.lambda_handler"
   architectures = ["x86_64"]
+  runtime       = "python3.8"
+  package_type  = "Zip"
   timeout       = 300
   s3_bucket     = "snowbit-shared-resources"
-  s3_key        = "lambda/ecr-findings.zip"
+  s3_key        = "lambda/ecr-image-scan.zip"
+  layers        = [aws_lambda_layer_version.requests.arn]
+  tags          = merge(var.additional_tags, {
+    terraform_execution_id = random_string.this.id
+  })
   environment {
     variables = {
       PRIVATE_KEY      = var.private_key
       APPLICATION_NAME = var.application_name
       SUBSYSTEM_NAME   = var.subsystem_name
-      ENDPOINT         = lookup(local.coralogix_domain_map, var.coralogix_domain)
+      CORALOGIX_DOMAIN = lookup(local.coralogix_domain_map, var.coralogix_domain)
     }
   }
-  tags = merge(var.additional_tags, {
-    terraform_execution_id = random_string.id.id
+}
+resource "aws_lambda_permission" "invoke" {
+  action         = "lambda:InvokeFunction"
+  function_name  = aws_lambda_function.this.function_name
+  principal      = "events.amazonaws.com"
+  statement_id   = "invoke-${random_string.this.result}"
+  source_arn     = aws_cloudwatch_event_rule.this.arn
+  source_account = data.aws_caller_identity.this.account_id
+}
+// EventBridge rule
+resource "aws_cloudwatch_event_rule" "this" {
+  name          = "ECR-image-scan-lambda-invoke-${random_string.this.id}"
+  event_pattern = <<EOF
+{
+  "source": ["aws.ecr"],
+  "detail-type": ["ECR Image Scan"],
+  "detail": {
+    "scan-status": ["COMPLETE"]
+  }
+}
+EOF
+  tags          = merge(var.additional_tags, {
+    terraform_execution_id = random_string.this.id
   })
 }
+resource "aws_cloudwatch_event_target" "this" {
+  rule      = aws_cloudwatch_event_rule.this.name
+  target_id = "eventbridge-target-${random_string.this.id}"
+  arn       = aws_lambda_function.this.arn
+}
+// IAM
 resource "aws_iam_role" "this" {
+  name               = "Lambda-role-ecr-image-scan-${random_string.this.id}"
   assume_role_policy = jsonencode({
     Version   = "2012-10-17"
     Statement = [
@@ -74,17 +115,17 @@ resource "aws_iam_role" "this" {
       "Version"   = "2012-10-17",
       "Statement" = [
         {
-          "Effect"   = "Allow",
-          "Action"   = "logs:CreateLogGroup",
-          "Resource" = "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.this.account_id}:*"
-        },
-        {
           "Effect" = "Allow",
           "Action" = [
+            "logs:CreateLogGroup",
             "logs:CreateLogStream",
             "logs:PutLogEvents"
-          ],
-          "Resource" = "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.this.account_id}:log-group:/aws/lambda/${lower(aws_lambda_function.this.function_name)}:*"
+          ]
+          "Resource" = [
+            "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.this.account_id}:*",
+            "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.this.account_id}:*:*"
+
+          ]
         }
       ]
     })
@@ -102,8 +143,12 @@ resource "aws_iam_role" "this" {
       ]
     })
   }
+  tags = merge(var.additional_tags, {
+    terraform_execution_id = random_string.this.id
+  })
 }
-resource "random_string" "id" {
+// Misc
+resource "random_string" "this" {
   length  = 6
   upper   = false
   special = false
--- SIEM & SaaS/AWS/ECR Findings to Coralogix/values.auto.tfvars
+++ SIEM & SaaS/AWS/ECR Findings to Coralogix/values.auto.tfvars
@@ -1,5 +1,7 @@
-private_key      = ""
-application_name = ""
-subsystem_name   = ""
-coralogix_domain = ""
-additional_tags  = {}
+private_key      = "39736fd1-22e2-bb84-ed21-c3a59785e097"
+application_name = "ecr-test-terra"
+subsystem_name   = "ecr-test-terra"
+coralogix_domain = "Europe"
+additional_tags  = {
+  Owner = "Nir Limor"
+}
