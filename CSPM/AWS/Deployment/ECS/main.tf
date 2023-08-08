f3e4dc6756d4aeb132df6c31a4703c6b1a65a45e
nirlimor <nir.limor@coralogix.com>
2023-08-08 12:36:48 +0300

    CSPM ECS

--- CSPM/AWS/Deployment/ECS/main.tf
+++ CSPM/AWS/Deployment/ECS/main.tf
@@ -257,6 +257,7 @@ data "aws_caller_identity" "this" {}
 resource "aws_cloudwatch_log_group" "this" {
   name = "/aws/ecs/${var.Cluster_Name}-${random_string.this.id}"
   tags = var.additional_tags
+  retention_in_days = 1
 }
 resource "aws_ecs_cluster" "this" {
   name = var.Cluster_Name
