9fad798150c318380ea73b25e56bde9333fad362
Nir Limor <nir.limor@coralogix.com>
2024-03-28 07:54:56 +0200

    CSPM Update

--- CSPM/AWS/Deployment/EC2/resources.tf
+++ CSPM/AWS/Deployment/EC2/resources.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "cspm-instance" {
-  ami                         = local.ubuntu-amis-map[data.aws_region.current.name]
-  instance_type               = length(var.instanceType) > 0 ? var.instanceType : "t3.small"
-  key_name                    = var.SSHKeyName
+  ami                         = data.aws_ami.ubuntu.image_id
+  instance_type               = length(var.AWS-instanceType) > 0 ? var.AWS-instanceType : "t3.small"
+  key_name                    = var.AWS-SSHKeyName
   iam_instance_profile        = aws_iam_instance_profile.CSPMInstanceProfile.id
-  associate_public_ip_address = var.public_instance
-  subnet_id                   = var.Subnet_ID
+  associate_public_ip_address = var.AWS-public_instance
+  subnet_id                   = var.AWS-Subnet_ID
   vpc_security_group_ids      = [
-    var.security_group_id != "" ? var.security_group_id : aws_security_group.CSPMSecurityGroup[0].id
+    var.AWS-security_group_id != "" ? var.AWS-security_group_id : aws_security_group.CSPMSecurityGroup[0].id
   ]
   user_data = <<EOT
 #!/bin/bash
@@ -15,15 +15,15 @@ ${local.docker_install}
 ${local.docker_command_in_cron}
 EOT
   root_block_device {
-    volume_type = length(var.DiskType) > 0 ? var.DiskType : "gp3"
-    encrypted   = var.ebs_encryption
+    volume_type = length(var.AWS-DiskType) > 0 ? var.AWS-DiskType : "gp3"
+    encrypted   = var.AWS-ebs_encryption
   }
   metadata_options {
     http_endpoint               = "enabled"
     http_tokens                 = "required"
     http_put_response_hop_limit = 2
   }
-  tags = merge(var.additional_tags,
+  tags = merge(var.AWS-additional_tags,
     {
       Name         = "Snowbit CSPM"
       Terraform-ID = random_string.id.id
@@ -32,18 +32,18 @@ EOT
 }
 resource "aws_security_group" "CSPMSecurityGroup" {
   name        = "CSPM-Security-Group-${random_string.id.id}"
-  count       = var.security_group_id == "" ? 1 : 0
+  count       = var.AWS-security_group_id == "" ? 1 : 0
   vpc_id      = data.aws_subnet.subnet.vpc_id
   description = "A security group for Snowbit CSPM"
-  tags        = merge(var.additional_tags,
+  tags        = merge(var.AWS-additional_tags,
     {
       Terraform-ID = random_string.id.id
     }
   )
   ingress {
-    description = var.SSHIpAddress == "0.0.0.0/0" ?  "SSH from the world" : length(var.SSHIpAddress) > 0 ? "SSH from user provided IP - ${var.SSHIpAddress}" : "SSH from the creators public IP - ${data.http.external-ip-address.response_body}/32"
+    description = var.AWS-SSHIpAddress == "0.0.0.0/0" ?  "SSH from the world" : length(var.AWS-SSHIpAddress) > 0 ? "SSH from user provided IP - ${var.AWS-SSHIpAddress}" : "SSH from the creators public IP - ${data.http.external-ip-address.response_body}/32"
     cidr_blocks = [
-      length(var.SSHIpAddress) > 0 ? var.SSHIpAddress : "${data.http.external-ip-address.response_body}/32"
+      length(var.AWS-SSHIpAddress) > 0 ? var.AWS-SSHIpAddress : "${data.http.external-ip-address.response_body}/32"
     ]
     from_port        = 22
     to_port          = 22
@@ -64,7 +64,7 @@ resource "aws_security_group" "CSPMSecurityGroup" {
 resource "aws_iam_instance_profile" "CSPMInstanceProfile" {
   name = "CSPM-Instance-Profile-${random_string.id.id}"
   role = aws_iam_role.CSPMRole.name
-  tags = merge(var.additional_tags,
+  tags = merge(var.AWS-additional_tags,
     {
       Terraform-ID = random_string.id.id
     }
@@ -93,7 +93,7 @@ resource "aws_iam_role" "CSPMRole" {
       policy = inline_policy.value["policy"]
     }
   }
-  tags = merge(var.additional_tags,
+  tags = merge(var.AWS-additional_tags,
     {
       Terraform-ID = random_string.id.id
     }
--- CSPM/AWS/Deployment/EC2/variables.tf
+++ CSPM/AWS/Deployment/EC2/variables.tf
@@ -1,111 +1,117 @@
-variable "additional_tags" {
+// AWS
+variable "AWS-additional_tags" {
   type    = map(string)
   default = {}
 }
-variable "instanceType" {
+variable "AWS-instanceType" {
   type    = string
   validation {
-    condition = length(var.instanceType) == 0 ? true : !can(regex("^(?:t[12]\\.(?:nano|micro|small)|c[67]g(?:d|n)?\\.medium|m[1367]g?d?\\.(?:small|medium)|r[67]gd?\\.medium|a1\\.medium|is4gen\\.medium|x2gd\\.medium)$", var.instanceType))
+    condition = length(var.AWS-instanceType) == 0 ? true : !can(regex("^(?:t[12]\\.(?:nano|micro|small)|c[67]g(?:d|n)?\\.medium|m[1367]g?d?\\.(?:small|medium)|r[67]gd?\\.medium|a1\\.medium|is4gen\\.medium|x2gd\\.medium)$", var.AWS-instanceType))
     error_message = "Invalid instance type"
   }
 }
-variable "Subnet_ID" {
+variable "AWS-Subnet_ID" {
   type        = string
   description = "Subnet for the EC2 instance"
   validation {
-    condition     = can(regex("^subnet-\\w+", var.Subnet_ID))
+    condition     = can(regex("^subnet-\\w+", var.AWS-Subnet_ID))
     error_message = "Invalid subnet ID"
   }
 }
-variable "SSHKeyName" {
+variable "AWS-SSHKeyName" {
   type        = string
   default     = ""
   description = "The key to SSH the CSPM instance"
 }
-variable "DiskType" {
+variable "AWS-DiskType" {
   type    = string
+  default = "gp3"
   validation {
-    condition = var.DiskType == "" ? true : can(regex("^(gp[23]|io[12])$", var.DiskType))
-    error_message = "Invalid disk type"
+    condition = var.AWS-DiskType == "" ? true : can(regex("^(gp[23]|io[12])$", var.AWS-DiskType))
+    error_message = "Invalid disk type."
   }
 }
-variable "SSHIpAddress" {
+variable "AWS-SSHIpAddress" {
   type = string
   description = "The public IP address for SSH access to the EC2 instance"
   validation {
-    condition = var.SSHIpAddress == "" ? true : can(regex("^(?:\\d{1,3}\\.){3}\\d{1,3}\\/\\d{1,2}$", var.SSHIpAddress))
-    error_message = "IP address is not valid - expected x.x.x.x/x"
+    condition = var.AWS-SSHIpAddress == "" ? true : can(regex("^(?:\\d{1,3}\\.){3}\\d{1,3}\\/\\d{1,2}$", var.AWS-SSHIpAddress))
+    error_message = "IP address is not valid - expected x.x.x.x/x ."
   }
 }
-variable "GRPC_Endpoint" {
-  type        = string
-  default     = "Europe"
-  description = "The address of the GRPC endpoint for the coralogix account"
-  validation {
-    condition     = can(regex("^(Europe|Europe2|India|Singapore|US)$", var.GRPC_Endpoint))
-    error_message = "Invalid GRPC endpoint"
-  }
-}
-variable "applicationName" {
-  type        = string
-  description = "Application name for Coralogix account (no spaces)"
-}
-variable "subsystemName" {
-  type        = string
-  description = "Subsystem name for Coralogix account (no spaces)"
-}
-variable "TesterList" {
-  type        = string
-  default     = ""
-  description = "Services for next scan"
-}
-variable "RegionList" {
-  type    = string
-  default = ""
-}
-variable "PrivateKey" {
-  type        = string
-  description = "The API Key from the Coralogix account"
-  sensitive   = true
-  validation {
-    condition     = can(regex("^\\w{8}-(?:\\w{4}-){3}\\w{12}$", var.PrivateKey))
-    error_message = "The PrivateKey should be valid UUID string"
-  }
-}
-variable "alertAPIkey" {
-  type        = string
-  description = "The Alert API key from the Coralogix account"
-  sensitive   = true
-  validation {
-    condition = var.alertAPIkey == "" ? true : can(regex("^\\w{8}-(?:\\w{4}-){3}\\w{12}$", var.alertAPIkey))
-    error_message = "The alertAPIkey should be valid UUID string"
-  }
-}
-variable "Company_ID" {
-  type        = string
-  description = "The Coralogix team company ID"
-  validation {
-    condition     = can(regex("^\\d{5,10}", var.Company_ID))
-    error_message = "Invalid Company ID"
-  }
-}
-variable "ebs_encryption" {
+variable "AWS-ebs_encryption" {
   type        = bool
   default     = false
   description = "Decide id the EBS volume of the CSPM should be encrypted"
 }
-variable "public_instance" {
+variable "AWS-public_instance" {
   type        = bool
   default     = true
   description = "Decide if the EC2 instance should pull a public IP address or not"
 }
-variable "security_group_id" {
+variable "AWS-security_group_id" {
   type        = string
   default     = ""
   description = "External security group to use instead of creating a new one"
 }
-variable "multiAccountsARNs" {
+
+// Coralogix
+variable "Coralogix-GRPC_Endpoint" {
+  type        = string
+  default     = "EU1"
+  description = "The address of the GRPC endpoint for the coralogix account"
+  validation {
+    condition     = can(regex("^(?:EU|AP|US)[12]$", var.Coralogix-GRPC_Endpoint))
+    error_message = "Invalid GRPC endpoint."
+  }
+}
+variable "Coralogix-applicationName" {
+  type        = string
+  description = "Application name for Coralogix account (no spaces)"
+}
+variable "Coralogix-subsystemName" {
+  type        = string
+  description = "Subsystem name for Coralogix account (no spaces)"
+}
+variable "Coralogix-PrivateKey" {
+  type        = string
+  description = "The API Key from the Coralogix account"
+  sensitive   = true
+  validation {
+    condition     = can(regex("^\\w{8}\\-(?:(?:\\w{4})\\-){3}\\w{12}|^(cxt[p|h]_[a-zA-Z0-9]{30})$", var.Coralogix-PrivateKey))
+    error_message = "Invalid Private Key."
+  }
+}
+variable "Coralogix-alertAPIkey" {
+  type        = string
+  description = "The Alert API key from the Coralogix account"
+  sensitive   = true
+  validation {
+    condition = var.Coralogix-alertAPIkey == "" ? true : can(regex("^\\w{8}-(?:\\w{4}-){3}\\w{12}$", var.Coralogix-alertAPIkey))
+    error_message = "The alertAPIkey should be valid UUID string"
+  }
+}
+variable "Coralogix-Company_ID" {
+  type        = string
+  description = "The Coralogix team company ID"
+  validation {
+    condition     = can(regex("^\\d{5,10}", var.Coralogix-Company_ID))
+    error_message = "Invalid Company ID"
+  }
+}
+
+// CSPM
+variable "CSPM-TesterList" {
+  type        = string
+  default     = ""
+  description = "Services for next scan"
+}
+variable "CSPM-multiAccountsARNs" {
   type        = list(string)
   default     = []
   description = "Optional - add the ARN for one additional account that you wish to scan - refer to the CSPM documentation https://coralogix.com/docs/cloud-security-posture-cspm/"
+}
+variable "CSPM-RegionList" {
+  type    = string
+  default = ""
 }
